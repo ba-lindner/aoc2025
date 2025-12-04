288c7cdec5d54a22081c0ce0fdfc4d795735d9da
Lindner, Bernhard <bernhard.lindner@ibykus.de>
2025-12-04 06:43:01 +0100

    feat: day 3

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,35 @@
-aoc::aoc!(part1, part2 as Map2D<bool>);
+aoc::aoc!(part1, part2 as Map2D<u8>);
 
 fn input(inp: String) -> Data {
-    inp.map().to(eq!('@'))
+    inp.map().to(ctd)
 }
 
-fn part1(inp: Data) -> usize {
-    inp.pos()
-        .filter(|pos| inp[pos])
-        .filter(|pos| {
-            ALL_DIRS_DIAG.count(|dir| inp.get2d(*pos, *dir).copied().unwrap_or_default()) < 4
-        })
-        .count()
-}
-
-fn part2(mut inp: Data) -> u64 {
+fn part1(inp: Data) -> u64 {
     let mut sum = 0;
-    let mut changed = true;
-    while changed {
-        changed = false;
-        for pos in inp.pos() {
-            if !inp[pos] {
-                continue;
-            }
-            let adj = ALL_DIRS_DIAG.count(|dir| inp.get2d(pos, *dir).copied().unwrap_or_default());
-            if adj < 4 {
-                sum += 1;
-                inp[pos] = false;
-                changed = true;
-            }
+    for line in &*inp {
+        let first = *line[..line.len() - 1].iter().max().unwrap();
+        let first_pos = line.iter().position(|&x| x == first).unwrap();
+        let second = *line[first_pos + 1..].iter().max().unwrap();
+        sum += (first * 10 + second) as u64;
+    }
+    sum
+}
+
+fn part2(inp: Data) -> u64 {
+    let mut sum = 0;
+    for line in &*inp {
+        let mut jolt = 0;
+        let mut curr_pos = 0;
+        for i in (0..12).rev() {
+            let digit = line[curr_pos..line.len() - i].iter().max().unwrap();
+            curr_pos += line[curr_pos..line.len() - i]
+                .iter()
+                .position(|x| x == digit)
+                .unwrap()
+                + 1;
+            jolt = jolt * 10 + (*digit as u64);
         }
+        sum += jolt;
     }
     sum
 }
